9cef0c0abda5da68287e9e9d3cde7b9fa32ae4ef
Saneef Ansari <hello@saneef.com>
2017-08-05 23:18:49 +0530

    fix(qgen): will get stuck in overwrite prompt if the file exists
    removes 'pinkie-promise' package

--- dist/lib/prompt-helpers.js
+++ dist/lib/prompt-helpers.js
@@ -1,39 +1,54 @@
 'use strict';
 
-const inquirer = require('inquirer');
-const Promise = require('pinkie-promise');
+let promptForOverwrite = (() => {
+	var _ref = _asyncToGenerator(function* (file) {
+		const answers = _inquirer2.default.prompt([{
+			type: 'expand',
+			message: `Do you want to overwrite the file ${file}: `,
+			name: 'overwrite',
+			choices: [{
+				key: 'y',
+				name: 'Overwrite',
+				value: _constants2.default.OVERWRITE
+			}, {
+				key: 'a',
+				name: 'Overwrite this one and all next',
+				value: _constants2.default.OVERWRITE_ALL
+			}, new _inquirer2.default.Separator(), {
+				key: 'x',
+				name: 'Abort',
+				value: _constants2.default.ABORT
+			}]
+		}]);
 
-const constants = require('../constants');
+		return answers.overwrite;
+	});
 
-const isFileOrDir = require('./file-helpers').isFileOrDir;
+	return function promptForOverwrite(_x) {
+		return _ref.apply(this, arguments);
+	};
+})();
 
-function promptForOverwrite(file) {
-	return inquirer.prompt([{
-		type: 'expand',
-		message: `Do you want to overwrite the file ${file}: `,
-		name: 'overwrite',
-		choices: [{
-			key: 'y',
-			name: 'Overwrite',
-			value: constants.OVERWRITE
-		}, {
-			key: 'a',
-			name: 'Overwrite this one and all next',
-			value: constants.OVERWRITE_ALL
-		}, new inquirer.Separator(), {
-			key: 'x',
-			name: 'Abort',
-			value: constants.ABORT
-		}]
-	}]).then(answers => answers.overwrite);
-}
+var _inquirer = require('inquirer');
+
+var _inquirer2 = _interopRequireDefault(_inquirer);
+
+var _constants = require('../constants');
+
+var _constants2 = _interopRequireDefault(_constants);
+
+var _fileHelpers = require('./file-helpers');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 function promptIfFileExists(file) {
 	let r;
-	if (isFileOrDir(file) === 'file') {
+	if ((0, _fileHelpers.isFileOrDir)(file) === 'file') {
 		r = promptForOverwrite(file);
 	} else {
-		r = Promise.resolve(constants.WRITE);
+		r = Promise.resolve(_constants2.default.WRITE);
 	}
 	return r;
 }
--- src/lib/prompt-helpers.js
+++ src/lib/prompt-helpers.js
@@ -1,37 +1,35 @@
 'use strict';
-const inquirer = require('inquirer');
-const Promise = require('pinkie-promise');
+import inquirer from 'inquirer';
 
-const constants = require('../constants');
+import constants from '../constants';
+import {isFileOrDir} from './file-helpers';
 
-const isFileOrDir = require('./file-helpers').isFileOrDir;
+async function promptForOverwrite(file) {
+	const answers = inquirer.prompt([{
+		type: 'expand',
+		message: `Do you want to overwrite the file ${file}: `,
+		name: 'overwrite',
+		choices: [
+			{
+				key: 'y',
+				name: 'Overwrite',
+				value: constants.OVERWRITE
+			},
+			{
+				key: 'a',
+				name: 'Overwrite this one and all next',
+				value: constants.OVERWRITE_ALL
+			},
+			new inquirer.Separator(),
+			{
+				key: 'x',
+				name: 'Abort',
+				value: constants.ABORT
+			}
+		]
+	}]);
 
-function promptForOverwrite(file) {
-	return inquirer.prompt([
-		{
-			type: 'expand',
-			message: `Do you want to overwrite the file ${file}: `,
-			name: 'overwrite',
-			choices: [
-				{
-					key: 'y',
-					name: 'Overwrite',
-					value: constants.OVERWRITE
-				},
-				{
-					key: 'a',
-					name: 'Overwrite this one and all next',
-					value: constants.OVERWRITE_ALL
-				},
-				new inquirer.Separator(),
-				{
-					key: 'x',
-					name: 'Abort',
-					value: constants.ABORT
-				}
-			]
-		}
-	]).then(answers => answers.overwrite);
+	return answers.overwrite;
 }
 
 function promptIfFileExists(file) {
