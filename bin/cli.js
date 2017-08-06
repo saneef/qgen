60230505b311fa64b8aa0f6e4be31de49484d5ec
Saneef Ansari <hello@saneef.com>
2017-08-06 11:41:17 +0530

    feat(cli): adds alias for directory and config cli options

--- bin/cli.js
+++ bin/cli.js
@@ -12,14 +12,19 @@ const cli = meow(`
 		$ qgen <template name> [dest] [arguments] [options]
 
 	${chalk.bold('Options')}
-	--directory=<dir>	Templates directory # Default: ./gqen-templates
-	--config=<path>	Path to the JSON config file # Default: ./qgen.json
+		-d, --directory <dir>	Templates directory (defaults to ./gqen-templates)
+		-c, --config <path>	Path to the JSON config file (defaults to ./qgen.json)
 
 	${chalk.bold('Examples')}
 		$ qgen post ${chalk.dim('# generates the post template in the current folder')}
 		$ qgen post ./pages ${chalk.dim('# generates the post template inside ./pages')}
 		$ qgen post ./pages --page-title "Hello World" ${chalk.dim('# generates the post template in inside ./pages with data field pageTitle="Hello World" to the template rendering engine')}
-`);
+`, {
+	alias: {
+		d: 'directory',
+		c: 'config'
+	}
+});
 
 /**
  * Displays list of available templates to stdout
