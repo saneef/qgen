679939f6c99d29a50d7c61c8fb11283d4d0a429e
Saneef Ansari <hello@saneef.com>
2018-05-06 01:39:55 +0530

    style(js): lints

--- dist/lib/qgen-error.js
+++ dist/lib/qgen-error.js
@@ -4,6 +4,7 @@ const util = require('util');
 const NestedError = require('nested-error-stacks');
 
 function QGenError(message, nested) {
+	// eslint-disable-next-line prefer-reflect
 	NestedError.call(this, message, nested);
 	Object.assign(this, nested, { message });
 }
